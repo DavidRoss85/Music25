c3ea8b9fdba0b24699d4f1a929547046e6861634
David Ross <officialdavidross@gmail.com>
2025-04-06 17:15:12 -0700

    Add JSONContainer, JSONParser classes

--- src/main/java/parser/JSONContainer.java
+++ src/main/java/parser/JSONContainer.java
@@ -1,4 +1,17 @@
 package parser;
 
-public class JSONContainer {
+
+import java.util.HashMap;
+
+public class JSONContainer extends HashMap<String,String>{
+
+  @Override
+  public String toString() {
+    StringBuffer sb = new StringBuffer();
+
+    for(String key : this.keySet()){
+      sb.append(key+": "+this.get(key)+"\n");
+    }
+    return sb.toString();
+  }
 }
--- src/main/java/parser/JSONParser.java
+++ src/main/java/parser/JSONParser.java
@@ -1,4 +1,59 @@
 package parser;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class JSONParser {
+
+  private static ArrayList<JSONContainer> extractJSONCommands(String jsonString){
+//    jsonString = "["
+//        + "{\"ObjectType\": \"Staff\", \"ObjectNo\": \"1\", \"Rest\": \"Add Note A\"},"
+//        + "{\"ObjectType\": \"Staff\", \"ObjectNo\": \"1\", \"Rest\": \"Add Note B\"},"
+//        + "{\"ObjectType\": \"Staff\", \"ObjectNo\": \"1\", \"Rest\": \"Add Note C\"},"
+//        + "{\"ObjectType\": \"Staff\", \"ObjectNo\": \"3\", \"Rest\": \"Remove Note 8\"},"
+//        + "{\"ObjectType\": \"Staff\", \"ObjectNo\": \"3\", \"Rest\": \"Remove Note 1\"},"
+//        + "{\"ObjectType\": \"Staff\", \"ObjectNo\": \"3\", \"Rest\": \"Add Notes Fur Elise 1-8\"}"
+//        + "]";
+    // List to store extracted objects
+    ArrayList<JSONContainer> JSONCommands = new ArrayList<>();
+
+    try {
+      ObjectMapper objectMapper = new ObjectMapper();
+      JsonNode rootNode = objectMapper.readTree(jsonString);
+
+
+      // Iterate through JSON array and extract each item into MusicObject instances
+      for (JsonNode node : rootNode) {
+
+        Iterator<String> fieldNames = node.fieldNames();
+        JSONContainer jsonContainer = new JSONContainer();
+
+        while (fieldNames.hasNext()) {
+          String fieldName = fieldNames.next();
+          jsonContainer.put(fieldName,node.get(fieldName).asText());
+        }
+        JSONCommands.add(jsonContainer);
+
+//        String objectType = node.get("ObjectType").asText();
+//        int objectNo = node.get("ObjectNo").asInt();
+//        String rest = node.get("Rest").asText();
+
+//        JSONCommands.add(rest);
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return JSONCommands;
+  }
+
+  public static void main(String[] args) {
+
+    ArrayList<JSONContainer> commands = JSONParser.extractJSONCommands("[{\"ObjectType\": \"Staff\"}]");
+    for(JSONContainer command : commands){
+      System.out.println(command);
+    }
+  }
 }
